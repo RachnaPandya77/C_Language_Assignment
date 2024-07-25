23242d9abefd2567df732bea6a3d21ac7b32879e
Rachna Pandya <ruchipandya20@gmail.comgit config --global user.email ruchipandya20@gmail.com>
2024-07-25 23:34:31 +0530

    first commit

---
+++ 13_ascii.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+
+int main(){
+
+    printf("Find character value from ascii");
+    
+    char ch='R';
+    int asciivalue=ch;
+
+    printf("The ascii value of %c is %d",ch,asciivalue);
+
+    return 0;
+
+}

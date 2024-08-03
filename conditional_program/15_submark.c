063ffa11140677e34822ae1889c0db22279e32b0
Rachna Pandya <ruchipandya20@gmail.comgit config --global user.email ruchipandya20@gmail.com>
2024-08-03 21:39:35 +0530

    first commit

--- conditional_program/15_submark.c
+++ conditional_program/15_submark.c
@@ -1,7 +1,7 @@
 // Write a C program to determine eligibility for admission to a professional
-//course based on the following criteria
+// course based on the following criteria
 
-//Eligibility Criteria :
+// Eligibility Criteria :
 //(1)  Marks in Maths >=65 and Marks in Phy >=55 and Marks in Chem>=50
 //(2)  Total in all three subject >=190 or
 //(3)  Total in Maths and Physics >=140
@@ -11,15 +11,15 @@
 //(3) Input the marks obtained in Mathematics :72
 //(4) Total marks of Maths, Physics and Chemistry :188
 //(5) Total marks of Maths and Physics : 137
-//    The candidate is not eligible
+//     The candidate is not eligible
 
+// total = math + phy + chem
+// math-phy = math + phy
 
-//total = math + phy + chem
-//math-phy = math + phy
+#include <stdio.h>
 
-#include<stdio.h>
-
-void main(){
+void main()
+{
     int physics, chemistry, maths, total, math_phy, candidate;
 
     printf("Input the marks obtained in Physics: ");
@@ -32,27 +32,25 @@ void main(){
     total = physics + chemistry + maths;
     math_phy = maths + physics;
 
-    printf("Total of subject is:%d\n",total);
-    printf("Total of Maths and physics is:%d\n",math_phy);
+    printf("Total of subject is:%d\n", total);
+    printf("Total of Maths and physics is:%d\n", math_phy);
 
     // && true if each one is true
     if (maths >= 65 && physics >= 55 && chemistry >= 50 && total >= 190)
-        {
-            candidate = 1;
-        }
+    {
+        candidate = 1;
+    }
     else if (math_phy >= 140)
-        {
-            candidate = 1;
-        }
+    {
+        candidate = 1;
+    }
 
     if (candidate)
-        {
-            printf("The candidate is eligible for addmission");
-        }
+    {
+        printf("The candidate is eligible for addmission");
+    }
     else
-        {
-            printf("The candidate is not eligible for addmission");
-        }
-
-
+    {
+        printf("The candidate is not eligible for addmission");
+    }
 }
--- conditional_program/integer.c
+++ conditional_program/integer.c
@@ -1,6 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void main(){
+void main()
+{
+    printf("Write a C program to accept two integers and check whether they are equal or not\n");
 
-    printf("hello");
+    int a, b;
+    printf("Enter first integers:");
+    scanf("%d", &a);
+    printf("Enter second integers:");
+    scanf("%d", &b);
+
+    if (a == b)
+    {
+        printf("integers are equal\n");
+    }
+    else
+    {
+        printf("integers are not equal\n");
+    }
 }

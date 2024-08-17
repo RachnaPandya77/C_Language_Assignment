3d5bf48549530300022e91c6ac5692e0a31a183a
Rachna Pandya <ruchipandya20@gmail.comgit config --global user.email ruchipandya20@gmail.com>
2024-08-18 01:37:00 +0530

    first commit

---
+++ Function & Array/12_student.c
@@ -0,0 +1,22 @@
+// WAP to accept 5 students name and store it in array in c
+
+#include <stdio.h>
+
+int main()
+{
+    char names[5][50];
+
+    printf("Enter 5 student names:\n");
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%s", names[i]);
+    }
+
+    printf("Student names:\n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%s\n", names[i]);
+    }
+
+    return 0;
+}

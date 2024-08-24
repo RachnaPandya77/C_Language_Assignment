7432dd981ea6554dbdd8fc581511d09c300fc930
Rachna Pandya <ruchipandya20@gmail.comgit config --global user.email ruchipandya20@gmail.com>
2024-08-24 20:48:37 +0530

    first commit

--- Looping_program/3_user.c
+++ Looping_program/3_user.c
@@ -1,39 +1,38 @@
-//WAP to take 10 no. Input from user find out below values
-//a. How many Even numbers are there
-//b. How many odd numbers are there
-//c. Sum of even numbers
-//d. Sum of odd numbers
+// WAP to take 10 no. Input from user find out below values
+// a. How many Even numbers are there
+// b. How many odd numbers are there
+// c. Sum of even numbers
+// d. Sum of odd numbers
 
 #include <stdio.h>
 
-void main() {
+void main()
+{
     int numbers[10];
-    int even, odd;
-    int even_sum, odd_sum;
-
+    int even = 0, odd = 0;
+    int even_sum = 0, odd_sum = 0;
 
     printf("Enter 10 numbers:\n");
 
-    for (int i = 1; i < 10; i++)
-        {
+    for (int i = 0; i < 10; i++)
+    {
         printf("Num %d: ", i);
         scanf("%d", &numbers[i]);
 
-
-        if (numbers[i] % 2 == 0) {
+        if (numbers[i] % 2 == 0)
+        {
             even++;
             even_sum += numbers[i];
-        } else {
+        }
+        else
+        {
             odd++;
             odd_sum += numbers[i];
         }
     }
 
-
     printf("Number of even numbers: %d\n", even);
     printf("Number of odd numbers: %d\n", odd);
     printf("Sum of even numbers: %d\n", even_sum);
     printf("Sum of odd numbers: %d\n", odd_sum);
-
-
 }
